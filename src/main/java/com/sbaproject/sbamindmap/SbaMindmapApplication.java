9c834b77af17f9f945925734412195da5299b280
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-10-02 16:03:04 +0700

    init commit

---
+++ src/main/java/com/sbaproject/sbamindmap/SbaMindmapApplication.java
@@ -0,0 +1,13 @@
+package com.sbaproject.sbamindmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaMindmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbaMindmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbaproject/sbamindmap/SbaMindmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbaproject.sbamindmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaMindmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
